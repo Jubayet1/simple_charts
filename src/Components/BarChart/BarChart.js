76e1f68e9302b533a863167ab25fff7ffb309d83
Meherab <jubayet0106@gmail.com>
2022-06-10 10:15:47 +0600

    all chart added

--- src/Components/BarChart/BarChart.js
+++ src/Components/BarChart/BarChart.js
@@ -1,11 +1,101 @@
 import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const BarChart = () => {
+const BarReChart = () => {
+    const data = [
+        {
+          name: 'January',
+          sale: 4000,
+          profit: 2400,
+          lose: 2400,
+        },
+        {
+          name: 'February',
+          sale: 3000,
+          profit: 1398,
+          lose: 2210,
+        },
+        {
+          name: 'March',
+          sale: 2000,
+          profit: 9800,
+          lose: 2290,
+        },
+        {
+          name: 'April',
+          sale: 2780,
+          profit: 3908,
+          lose: 2000,
+        },
+        {
+          name: 'June',
+          sale: 1890,
+          profit: 4800,
+          lose: 2181,
+        },
+        {
+          name: 'July',
+          sale: 2390,
+          profit: 3800,
+          lose: 2500,
+        },
+        {
+          name: 'August',
+          sale: 3490,
+          profit: 4300,
+          lose: 2100,
+        },
+        {
+          name: 'September',
+          sale: 2000,
+          profit: 9800,
+          lose: 2290,
+        },
+        {
+          name: 'October',
+          sale: 3490,
+          profit: 4300,
+          lose: 2100,
+        },
+        {
+          name: 'November',
+          sale: 4000,
+          profit: 2400,
+          lose: 2400,
+        },
+        {
+          name: 'December',
+          sale: 1890,
+          profit: 4800,
+          lose: 2181,
+        }
+      ];
     return (
-        <div>
-            <p>Bar Chart</p>
+        <div className="mt-20">
+            <ResponsiveContainer width="100%" aspect={3}>
+                <BarChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                    top: 20,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="profit" stackId="a" fill="#8884d8" />
+                    <Bar dataKey="lose" stackId="a" fill="#82ca9d" />
+                    <Bar dataKey="sale" fill="#ffc658" />
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
 
-export default BarChart;
+export default BarReChart;
--- src/Components/RadarChart/RaderChart.js
+++ src/Components/RadarChart/RaderChart.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
 
 const RaderChart = () => {
+    const data = [
+        {
+          subject: 'Math',
+          A: 120,
+          B: 110,
+          fullMark: 150,
+        },
+        {
+          subject: 'Bengali',
+          A: 98,
+          B: 130,
+          fullMark: 150,
+        },
+        {
+          subject: 'English',
+          A: 86,
+          B: 130,
+          fullMark: 150,
+        },
+        {
+          subject: 'Science',
+          A: 99,
+          B: 100,
+          fullMark: 150,
+        },
+        {
+          subject: 'Physics',
+          A: 85,
+          B: 90,
+          fullMark: 150,
+        },
+        {
+          subject: 'History',
+          A: 65,
+          B: 85,
+          fullMark: 150,
+        },
+      ];
     return (
-        <div>
-            <p>Rader Chart</p>
+        <div className="mt-20">
+            <ResponsiveContainer width="100%" aspect={3}>
+        <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
+          <PolarGrid />
+          <PolarAngleAxis dataKey="subject" />
+          <PolarRadiusAxis />
+          <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+        </RadarChart>
+      </ResponsiveContainer>
         </div>
     );
 };
